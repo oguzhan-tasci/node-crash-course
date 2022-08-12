be59d693f34b6f8f92155ff880e95e02d988f408
oyeProjects <oyeprojects@hotmail.com>
2022-08-12 18:55:38 +0300

    created server

--- server.js
+++ server.js
@@ -4,7 +4,8 @@ const server = http.createServer((req, res) => {
   console.log('request made');
 });
 
+const port = 3000;
 // localhost is the default value for 2nd argument
-server.listen(3000, 'localhost', () => {
-  console.log('listening for requests on port 3000');
+server.listen(port, 'localhost', () => {
+  console.log(`listening for requests on port ${port}`);
 });
